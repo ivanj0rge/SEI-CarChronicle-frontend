a8ce15a941ce7c167f6da5a83d21c958ad74d383
ivanjorge <ivandcarvalho29@gmail.com>
2023-11-24 16:15:40 +0000

    final css

--- src/components/UserVehicles/UserVehicles.jsx
+++ src/components/UserVehicles/UserVehicles.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './UserVehicles.css';
 
@@ -18,6 +18,7 @@ function UserVehicles({ userVehicles }) {
     const [showHistory, setShowHistory] = useState(false);
     const [showAddHistory, setShowAddHistory] = useState(false);
     const [selectedVehicle, setSelectedVehicle] = useState(null);
+    const userId = localStorage.getItem("user_url");
     const [history, setHistory] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -29,6 +30,31 @@ function UserVehicles({ userVehicles }) {
         description: '',
     });
 
+    useEffect(() => {
+        const fetchUserVehicles = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/vehicles/?current_owner=${userId}`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                });
+                if (response.data.length > 0) {
+                    setNewHistory(prevState => ({
+                        ...prevState,
+                        vehicle: response.data[0].registrationNumber,
+                    }));
+                }
+                setSelectedVehicle(response.data[0].registrationNumber)
+                console.log(response.data[0].registrationNumber);
+            } catch (error) {
+                console.error('Error fetching user vehicles:', error);
+            }
+        };
+
+        fetchUserVehicles();
+    }, [apiUrl, token, userId]);
+
+
     const handleToggleHistory = async (vehicleId) => {
         if (showHistory) {
             setShowHistory(false);
@@ -38,160 +64,166 @@ function UserVehicles({ userVehicles }) {
         fetchHistory(vehicleId);
     }
 
-const fetchHistory = async (vehicleId) => {
-    try {
-        setLoading(true)
-        const response = await axios.get(`${apiUrl}/vehicles/${vehicleId}/history/`);
-        setHistory(response.data);
-        setLoading(false);
-    } catch (error) {
-        console.error('Error fetching history:', error);
-        setError('Error fetching history. Please try again.');
-        setLoading(false);
-    }
-};
+    const fetchHistory = async (vehicleId) => {
+        try {
+            setLoading(true)
+            const selectedVehicle = userVehicles.find(vehicle => vehicle.registrationNumber === vehicleId);
+            setSelectedVehicle(selectedVehicle)
+            console.log(selectedVehicle);
+            const response = await axios.get(`${apiUrl}/vehicles/${selectedVehicle}/history/`);
+            setHistory(response.data);
+            setLoading(false);
+        } catch (error) {
+            console.error('Error fetching history:', error);
+            setError('Error fetching history. Please try again.');
+            setLoading(false);
+        }
+    };
 
-// const handleRemove = async (vehicleId) => {
-//     try {
-//         await axios.put(`${apiUrl}/vehicles/${vehicleId}/`,
-//             { current_owner: null },
-//             {
-//                 headers: {
-//                     'Authorization': `Bearer ${token}`,
-//                     'Content-Type': 'application/json',
-//                 },
-//             });
+    // const handleRemove = async (vehicleId) => {
+    //     try {
+    //         await axios.put(`${apiUrl}/vehicles/${vehicleId}/`,
+    //             { current_owner: null },
+    //             {
+    //                 headers: {
+    //                     'Authorization': `Bearer ${token}`,
+    //                     'Content-Type': 'application/json',
+    //                 },
+    //             });
 
-//     } catch (error) {
-//         console.error('Error removing vehicle owner:', error.message);
-//     }
-// };
+    //     } catch (error) {
+    //         console.error('Error removing vehicle owner:', error.message);
+    //     }
+    // };
 
-const handleCreateHistory = async () => {
-    try {
-        const response = await axios.post(`${apiUrl}/vehicles/${selectedVehicle.registrationNumber}/history/`, {
-            ...newHistory,
-            vehicle_registration_number: selectedVehicle.registrationNumber,
-            vehicle: selectedVehicle.registrationNumber,
-        });
-        setHistory([...history, response.data]);
-        setNewHistory({
-            vehicle_registration_number: selectedVehicle.registrationNumber,
-            vehicle: selectedVehicle.registrationNumber,
-            date: '',
-            mileage: 0,
-            service_type: '',
-            description: '',
-        });
+    const handleCreateHistory = async () => {
+        try {
+            const response = await axios.post(`${apiUrl}/vehicles/${selectedVehicle}/history/`, {
+                ...newHistory,
+                vehicle_registration_number: selectedVehicle,
+                vehicle: selectedVehicle,
+            });
+            setHistory([...history, response.data]);
+            setNewHistory({
+                vehicle_registration_number: selectedVehicle,
+                vehicle: selectedVehicle,
+                date: '',
+                mileage: 0,
+                service_type: '',
+                description: '',
+            });
 
-        setShowAddHistory(false)
+            setShowAddHistory(false)
 
-    } catch (error) {
-        console.error('Error creating history:', error);
-    }
-};
+        } catch (error) {
+            console.error('Error creating history:', error);
+        }
+    };
 
-const handleDeleteHistory = async (historyId) => {
-    try {
-        await axios.delete(`${apiUrl}/history/${historyId}/`);
-        setHistory(history.filter((h) => h.history_id !== historyId));
-    } catch (error) {
-        console.error('Error deleting history:', error);
-    }
-};
+    const handleDeleteHistory = async (historyId) => {
+        try {
+            await axios.delete(`${apiUrl}/history/${historyId}/`);
+            setHistory(history.filter((h) => h.history_id !== historyId));
+        } catch (error) {
+            console.error('Error deleting history:', error);
+        }
+    };
 
-// if (loading) {
-//     return <div className='loading'>Loading...</div>;
-// }
+    // if (loading) {
+    //     return <div className='loading'>Loading...</div>;
+    // }
 
-// if (error) {
-//     return <div className='error'>{error}</div>;
-// }
+    // if (error) {
+    //     return <div className='error'>{error}</div>;
+    // }
 
-return (
-    <div className='card-page'>
-        <div className="vehicle-card">
-            {userVehicles.map((vehicle) => (
-                <div key={vehicle.registrationNumber} className="user-vehicle">
-                    <div className="registration">
-                        {vehicle.registrationNumber}
+    return (
+        <div className='card-page'>
+            <div className="vehicle-card">
+                {userVehicles.map((vehicle) => (
+                    <div key={selectedVehicle} className="user-vehicle">
+                        <div className="registration">
+                            {selectedVehicle}
+                        </div>
+                        <div className="details">Make: <span>{vehicle.make}</span></div>
+                        <div className="details">Model: <span>{vehicle.model}</span></div>
+                        <div className="details">Colour: <span>{vehicle.colour}</span></div>
+                        <div className="details">Year: <span>{vehicle.yearOfManufacture}</span></div>
+                        <div className="details">Fuel: <span>{vehicle.fuel_type}</span></div>
+                        <div className="details">Engine: <span>{vehicle.engineCapacity}</span></div>
+                        {/* <div className="details">Previous Reg Owners: <span>{vehicle.previous_owners_count}</span></div> */}
+                        {/* <button onClick={() => handleRemove(vehicle.registrationNumber)}>Remove</button> */}
+                        <button className='history' onClick={() => handleToggleHistory(!handleToggleHistory)}>
+                            {showHistory ? 'Hide History' : 'See History'}
+                        </button>
                     </div>
-                    <div className="details">Make: <span>{vehicle.make}</span></div>
-                    <div className="details">Model: <span>{vehicle.model}</span></div>
-                    <div className="details">Colour: <span>{vehicle.colour}</span></div>
-                    <div className="details">Year: <span>{vehicle.yearOfManufacture}</span></div>
-                    <div className="details">Fuel: <span>{vehicle.fuel_type}</span></div>
-                    <div className="details">Engine: <span>{vehicle.engineCapacity}</span></div>
-                    {/* <div className="details">Previous Reg Owners: <span>{vehicle.previous_owners_count}</span></div> */}
-                    {/* <button onClick={() => handleRemove(vehicle.registrationNumber)}>Remove</button> */}
-                    <button className='history' onClick={() => handleToggleHistory(!handleToggleHistory)}>
-                        {showHistory ? 'Hide History' : 'See History'}
-                    </button>
+                ))}
+            </div>
+
+            {showHistory && (
+                <div>
+                    {loading && <div className='loading'>Loading...</div>}
+                    {error && (
+                        <div className='error'>
+                            Error: {error}. Please try again.
+                            {console.error(error)}
+                        </div>
+                    )}
+                    {!loading && !error && (
+                        <div>
+                            <h2>Vehicle History</h2>
+                            <ul>
+                                {history.map((h) => (
+                                    <li key={h.history_id}>
+                                        {h && h.service_type && (
+                                            <span>{`${SERVICE_TYPES[h.service_type]} on ${h.date}.`}</span>
+                                        )}
+                                        <br />
+                                        {`${h.description}`}
+                                        <button onClick={() => handleDeleteHistory(h.history_id)}>Delete</button>
+                                    </li>
+                                ))}
+                            </ul>
+
+                            <button onClick={() => setShowAddHistory(!showAddHistory)}>
+                                {showAddHistory ? 'Hide' : 'Add New Records'}
+                            </button>
+
+                            {showAddHistory && (
+                                <div>
+                                    <h3>Add New Record</h3>
+                                    <div>
+                                        <label>Date:</label>
+                                        <input type='date' value={newHistory.date} onChange={(e) => setNewHistory({ ...newHistory, date: e.target.value })} />
+                                    </div>
+                                    <div>
+                                        <label>Mileage:</label>
+                                        <input type='number' value={newHistory.mileage} onChange={(e) => setNewHistory({ ...newHistory, mileage: e.target.value })} />
+                                    </div>
+                                    <div>
+                                        <label>Service Type:</label>
+                                        <select value={newHistory.service_type} onChange={(e) => setNewHistory({ ...newHistory, service_type: e.target.value })}>
+                                            {Object.keys(SERVICE_TYPES).map((type) => (
+                                                <option key={type} value={type}>
+                                                    {SERVICE_TYPES[type]}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label>Description:</label>
+                                        <textarea value={newHistory.description} onChange={(e) => setNewHistory({ ...newHistory, description: e.target.value })} />
+                                    </div>
+                                    <button onClick={handleCreateHistory}>Create Record</button>
+                                </div>
+                            )}
+                        </div>
+                    )}
                 </div>
-            ))}
+            )}
         </div>
 
-        {showHistory && (
-            <div>
-                {loading && <div className='loading'>Loading...</div>}
-                {error && (
-                    <div className='error'>
-                        Error: {error}. Please try again.
-                        {console.error(error)}
-                    </div>
-                )}
-                {!loading && !error && (
-                    <div>
-                        <h2>Vehicle History</h2>
-                        <ul>
-                            {history.map((h) => (
-                                <li key={h.history_id}>
-                                    {`${SERVICE_TYPES[h.service_type]} on ${h.date}.`} <br />
-                                    {`${h.description}`}
-                                    <button onClick={() => handleDeleteHistory(h.history_id)}>Delete</button>
-                                </li>
-                            ))}
-                        </ul>
-
-                        <button onClick={() => setShowAddHistory(!showAddHistory)}>
-                            {showAddHistory ? 'Hide' : 'Add New Records'}
-                        </button>
-
-                        {showAddHistory && (
-                            <div>
-                                <h3>Add New Record</h3>
-                                <div>
-                                    <label>Date:</label>
-                                    <input type='date' value={newHistory.date} onChange={(e) => setNewHistory({ ...newHistory, date: e.target.value })} />
-                                </div>
-                                <div>
-                                    <label>Mileage:</label>
-                                    <input type='number' value={newHistory.mileage} onChange={(e) => setNewHistory({ ...newHistory, mileage: e.target.value })} />
-                                </div>
-                                <div>
-                                    <label>Service Type:</label>
-                                    <select value={newHistory.service_type} onChange={(e) => setNewHistory({ ...newHistory, service_type: e.target.value })}>
-                                        {Object.keys(SERVICE_TYPES).map((type) => (
-                                            <option key={type} value={type}>
-                                                {SERVICE_TYPES[type]}
-                                            </option>
-                                        ))}
-                                    </select>
-                                </div>
-                                <div>
-                                    <label>Description:</label>
-                                    <textarea value={newHistory.description} onChange={(e) => setNewHistory({ ...newHistory, description: e.target.value })} />
-                                </div>
-                                <button onClick={handleCreateHistory}>Create Record</button>
-                            </div>
-                        )}
-                    </div>
-                )}
-            </div>
-        )}
-    </div>
-
-)
+    )
 }
 
 export default UserVehicles;
